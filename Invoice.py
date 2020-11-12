2caf8bcb1633c4fe81edcd92c1fc4db53c96faf4
DerekSeav <drseav@gmail.com>
2020-11-11 20:33:02 -0500

    first commit

---
+++ Invoice.py
@@ -0,0 +1,58 @@
+class Invoice:
+
+    def __init__(self):
+        self.items = {}
+
+    def addProduct(self, qnt, price, discount):
+        self.items['qnt'] = qnt
+        self.items['unit_price'] = price
+        self.items['discount'] = discount
+        return self.items
+
+    def totalImpurePrice(self, products):
+        total_impure_price = 0
+        for k, v in products.items():
+            total_impure_price += float(v['unit_price']) * int(v['qnt'])
+        total_impure_price = round(total_impure_price, 2)
+        return total_impure_price
+
+    def totalDiscount(self, products):
+        total_discount = 0
+        for k, v in products.items():
+            total_discount += (int(v['qnt']) * float(v['unit_price'])) * float(v['discount']) / 100
+        total_discount = round(total_discount, 2)
+        self.total_discount = total_discount
+        return total_discount
+
+    def partialDiscount(self, products):
+        partial_disc = 0
+        for k, v in products.items():
+            partial_disc += (int(v['qnt']) * float(v['unit_price'])) * float(v['discount']) / 200
+        partial_disc = round(partial_disc, 2)
+        self.partial_disc = partial_disc
+        return partial_disc
+
+    def totalPurePrice(self, products):
+        total_pure_price = self.totalImpurePrice(products) - self.totalDiscount(products)
+        return total_pure_price
+
+    def halfPurePrice(self, products):
+        half_pure_price = self.totalImpurePrice(products) - self.partialDiscount(products)
+        return half_pure_price
+
+    def inputAnswer(self, input_value):
+        while True:
+            userInput = input(input_value)
+            if userInput in ['y', 'n']:
+                return userInput
+            print("y or n! Try again.")
+
+    def inputNumber(self, input_value):
+        while True:
+            try:
+                userInput = float(input(input_value))
+            except ValueError:
+                print("Not a number! Try again.")
+                continue
+            else:
+                return userInput
